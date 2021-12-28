81c2f84fb112305904163491f233b73a286c88e8
Samarth <sethisamarth1@gmail.com>
2021-12-28 18:43:30 +0530

    first

---
+++ TodoApi/TodoApi/Controllers/EmployController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployAController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Details")]
+        public IActionResult GetEmployData()
+        {
+            List<string> employData = new List<string>()
+            {
+                "sam ", "shiv" ,"sid", "ram"
+            };
+          return this.Ok(new {Succuss=true,message="get data successful",Data =employData});
+        }
+        [HttpGet("Addresses")]
+        public IActionResult GetEmployAddress()
+        {
+            List<string> employData = GetAddress();
+            return this.Ok(new { Succuss = true, message = "get address successful", Data = employData });
+        }
+
+        private static List<string> GetAddress()
+        {
+            return new List<string>()
+            {
+                "jabalpur", "delhi" ,"vijay nagar", "noida"
+            };
+        }
+        [HttpPost("{Address}")]
+        public IActionResult RegisterEmployAddress(string Address)
+        {
+            List<string> employData = GetAddress();
+            employData.Add(Address);
+            return this.Ok(new { Succuss = true, message = "Register Address successful", Data = employData });
+        }
+    }
+}
